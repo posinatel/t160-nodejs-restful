eeeb2d632d2dad63cf1678a2a6d6ac4eebbd113c
Aluno do Inatel <aluno@local.inatel.br>
2019-08-17 13:41:34 -0300

    Add tasks' route to app.js

--- app.js
+++ app.js
@@ -4,10 +4,6 @@ const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
 
-app.use((request, response, next) => {
-  response.status(200).json({
-    message: 'It just works baby!!'
-  })
-});
+app.use('/api/tasks', require('./api/routes/tasks'));
 
 module.exports = app;
