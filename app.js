e2bb910c9c977016fd2b309ae2d9d4faeb7a2fd5
Edy Segura <edysegura@gmail.com>
2019-08-16 23:19:14 -0300

    Update app.js to use tasks' route

--- app.js
+++ app.js
@@ -2,14 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-
 app.use(morgan('dev'));
 
-app.use((request, response, next) => {
-  console.log(request.url)
-  response.status(200).json({
-    message: 'It works baby!'
-  });
-});
+app.use('/api/tasks', require('./api/routes/tasks'));
 
 module.exports = app;
