43bdb1ad33aa04def8de718c628753e7964ae9e8
Aluno do Inatel <aluno@local.inatel.br>
2019-08-17 14:26:44 -0300

    Add others HTTP routes

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,21 +2,35 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (request, response) => {
-  response.status(200).json({
-    message: 'Handling POST requests to /tasks'
+  response.status(201).json({
+    message: 'The task has been created'
   });
 });
 
 router.get('/', (request, response) => {
   response.status(200).json({
-    message: 'Handling GET requests to /tasks'
+    message: 'Tasks has been fetched'
   });
 });
 
-router.get('/:tasksId', (request, response) => {
-  const id = request.params.tasksId;
+router.get('/:taskId', (request, response) => {
+  const id = request.params.taskId;
   response.status(200).json({
     message: `Task with ID = ${id} was fetched`
+  });
+});
+
+router.patch('/:taskId', (request, response) => {
+  const id = request.params.taskId;
+  response.status(200).json({
+    message: `Task with ID = ${id} was updated`
+  });
+});
+
+router.delete('/:taskId', (request, response) => {
+  const id = request.params.taskId;
+  response.status(200).json({
+    message: `Task with ID = ${id} was deleted`
   });
 });
 
