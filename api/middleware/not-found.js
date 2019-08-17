c43ea078ee861f9c4fa4948d78e9f7320cf6a624
Edy Segura <edysegura@gmail.com>
2019-08-17 01:03:38 -0300

    Update not-found.js

--- api/middleware/not-found.js
+++ api/middleware/not-found.js
@@ -1,6 +1,6 @@
 const notFound = (request, response, next) => {
   response.status(404).json({
-    message: `Route to ${request.method} ${request.url} not found`
+    message: `${request.method} ${request.url} not found`
   });
 }
 
